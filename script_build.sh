35ae5a4ff9c99faa85aee8b0c1817354ca8331c7
drohan898 <drohan940@gmail.com>
2017-06-25 12:21:11 +0530

    Initial push

---
+++ script_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+# ccache
+export USE_CCACHE=1
+export CCACHE_DIR=//home/ccache/rohan  
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+
+export KBUILD_BUILD_USER=#Rohan
+export KBUILD_BUILD_HOST=UbuntuZeus
+
+#Clean
+make  clean && make clobber
+
+# Killing...
+#prebuilts/sdk/tools/jack-admin kill-server
+# Starting...
+#prebuilts/sdk/tools/jack-admin start-server
+
+# build
+. build/envsetup.sh
+# make -j8 libinit_msm8916
+#export WITH_MAGISK=true
+#export WITH_SU=true
+#lunch xhd_lettuce-userdebug
+breakfast lettuce
+make bacon -j8
