380b3a82d16a7b7c5ef7fa0d954f2c1bb7fc841b
#Rohan <drohan940@gmail.com>
2017-11-04 06:50:42 -0700

    Update script & fix derps

--- script_build.sh
+++ script_build.sh
@@ -2,7 +2,7 @@
  
 # ccache
 export USE_CCACHE=1
-export CCACHE_DIR=//home/ccache/rohan  
+export CCACHE_DIR=/home/ccache/rohan  
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
 export KBUILD_BUILD_USER=#Rohan
@@ -12,17 +12,16 @@ export KBUILD_BUILD_HOST=UbuntuZeus
 make  clean && make clobber
 
 # Killing...
-#prebuilts/sdk/tools/jack-admin kill-server
+prebuilts/sdk/tools/jack-admin kill-server
 # Starting...
-#prebuilts/sdk/tools/jack-admin start-server
+prebuilts/sdk/tools/jack-admin start-server
 
 # build
 . build/envsetup.sh
-# make -j8 libinit_msm8916
 #export WITH_MAGISK=true
 #export WITH_SU=true
-export OTA_TYPE=Official
+#export OTA_TYPE=Official
 #export OTA_TYPE=experimental
-#lunch xhd_lettuce-userdebug
-breakfast lettuce
+#breakfast lettuce
+lunch lineage_$device-userdebug
 make bacon -j8
